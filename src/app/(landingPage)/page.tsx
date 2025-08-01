58c67589c3b48d5f5784c8262a03173f040fae5f
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-08-01 11:22:05 +0530

    Setup Clerk

--- src/app/(landingPage)/page.tsx
+++ src/app/(landingPage)/page.tsx
@@ -1,11 +1,17 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { SignedIn, useAuth } from "@clerk/nextjs";
 
 export default function Home() {
+  const user = useAuth();
   return (
     <div className="bg-cyan-100">
       <h1 className="h-16 w-16 text-black">Hello</h1>
       <Button variant={"secondary"}>Btn</Button>
+
+      <SignedIn>
+        <h1>Your User Id is :{user.userId} </h1>
+      </SignedIn>
     </div>
   );
 }
