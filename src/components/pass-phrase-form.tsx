3739b6debdb73e4892685d8057131ec78d9d56dd
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-08-02 15:20:38 +0530

    chore: Update comments in PassPhraseForm for clarity on user flow

--- src/components/pass-phrase-form.tsx
+++ src/components/pass-phrase-form.tsx
@@ -7,5 +7,26 @@ export const PassPhraseForm = ({
   userId,
   setIsPhraseVerified,
 }: PassPhraseFormProps) => {
+  /*
+ Flow 
+
+ 1. Check if user Exists  and get the data in the Convex DB 
+
+ 2. User Exists :
+    -Display him the Pass Phrase Component
+    -Verify The pass Phrase 
+    -If correct setPassPhrase verifed to be true
+
+
+ 3. User Does not Exist : 
+    -Display him the Generate Pass Phrase Component
+    -Create User
+    -trigger refresh or any thing that might be better
+    
+
+
+
+*/
+
   return <h1>Pass Phrase from</h1>;
 };
