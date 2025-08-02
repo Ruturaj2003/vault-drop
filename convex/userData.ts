6cfe0f0f462fa127120ebce92c5463b33dbe4c78
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-08-02 15:15:05 +0530

    Start the Pass Phrase Form from scratch

--- convex/userData.ts
+++ convex/userData.ts
@@ -1,5 +1,26 @@
-import { mutation } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 export const createUser = mutation({
-  handler: () => {},
+  args: { userId: v.string(), passPhrase: v.string() },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("userData", {
+      id: args.userId,
+      passPhrase: args.passPhrase,
+    });
+  },
+});
+
+export const getUserData = query({
+  args: { userId: v.string() },
+  handler: async (ctx, args) => {
+    const user = await ctx.db
+      .query("userData")
+      .filter((user) => {
+        return user.eq(user.field("id"), args.userId);
+      })
+      .first();
+
+    return user;
+  },
 });
