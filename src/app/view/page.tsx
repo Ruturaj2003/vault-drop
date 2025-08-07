674b83330f3bd9923ea73d19ed8b5a116aa95773
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-08-07 10:25:28 +0530

    Clean up and start again

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -1,50 +1,31 @@
 "use client";
+
 import { LoadingSpinner } from "@/components/loading-spinner";
 import useFileDataStore from "@/store/fileDataStore";
 import useMousePosition from "@/utils/useMousePosition";
 import { useEffect, useState } from "react";
 
 const ViewFilePage = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const { x, y } = useMousePosition();
   const getCurrentFile = useFileDataStore((state) => state.getCurrentFile);
 
   const [fileData, setFileData] = useState(() => getCurrentFile());
-  const [pdfData, setPdfData] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [dummyPdfData, setDummyPdfData] = useState<string | null>(null);
-
-  const [realPdfData, setRealPdfData] = useState<string | null>(null);
-  // API Call To get File
-  const { x, y } = useMousePosition();
   useEffect(() => {
     const file = getCurrentFile();
     setFileData(file);
-    fetch("/api/downloadFile", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        dummyUrl: fileData?.dummyFileUrl,
-        realUrl: fileData?.realFileUrl,
-      }),
-    })
-      .then((res) => res.json())
-      .then(({ dummyBuffer, realBuffer }) => {
-        setDummyPdfData(dummyBuffer);
-        setRealPdfData(realBuffer);
-        setPdfData(dummyBuffer);
-      });
-
     setIsLoading(false);
-  }, [getCurrentFile, fileData]);
+  }, [getCurrentFile]);
 
   if (isLoading) return <LoadingSpinner />;
   if (!fileData) return <p>No file selected.</p>;
 
   return (
-    <div className="flex flex-col gap-y4">
-      <h1 className="text-md ">This is your Mouse . </h1>
-      <h1 className="text-md ">
-        X:{x} y:{y}
+    <div className="flex flex-col gap-y-4">
+      <h1 className="text-md">This is your Mouse.</h1>
+      <h1 className="text-md">
+        X: {x} Y: {y}
       </h1>
     </div>
   );
